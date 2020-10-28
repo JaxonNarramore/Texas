703d9130feb894594e7e3353150569aee2dd801e
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-10-28 08:59:54 -0500

    [app.js] Add non-working function

---
+++ app.js
@@ -0,0 +1,37 @@
+// const form = document.createElement('form');
+// const divElement = document.createElement('div');
+// divElement.classList.add('form-group');
+// const emailLabel = document.createElement('label');
+// emailLabel.setAttribute('for', 'exampleFormControlInput1');
+// emailLabel.textContent = 'Your name';
+// // append to div
+// divElement.appendChild(emailLabel);
+
+
+
+
+// {/* <div class="form-group">
+//           <label for="exampleFormControlInput1">Your name</label>
+//           <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Your Name Here"></input> */}
+
+
+
+
+
+//           const option = document.createElement('form');
+//           const DivE = document.createElement('div');
+//           divE.classlist.add('form-group');
+//           const list = document.createElement('label');
+//           list.setAttribute('for', 'exampleFormControlInput1')
+
+
+
+
+
+
+
+
+
+        //   <div class="form-group">
+        //   <label for="exampleFormControlSelect1">What state do you think is better?</label>
+        //   <select class="form-control" id="exampleFormControlSelect1"></select>
